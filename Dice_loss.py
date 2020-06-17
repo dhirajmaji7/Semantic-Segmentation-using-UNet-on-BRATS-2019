9c1cb7ceae30ad0e31b07418bc99f23edcb65915
dhirajmaji7 <47034020+dhirajmaji7@users.noreply.github.com>
2020-06-18 01:27:34 +0530

    Update Dice_loss.py

--- Dice_loss.py
+++ Dice_loss.py
@@ -1,17 +1,20 @@
 # Computing the Dice Scores for individual slices and then the Mean Dice Score and Dice Loss
 
+# For non-tumor and background region:
 def dice_coef_0(y_true, y_pred,smooth=0.000001):
     y_true_f = K.flatten(y_true[:,:,:,0])
     y_pred_f = K.flatten(y_pred[:,:,:,0])
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
+# For necrosis:
 def dice_coef_1(y_true, y_pred,smooth=0.000001):
     y_true_f = K.flatten(y_true[:,:,:,1])
     y_pred_f = K.flatten(y_pred[:,:,:,1])
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
+# For edema:
 def dice_coef_2(y_true, y_pred,smooth=0.000001):
     y_true_f = K.flatten(y_true[:,:,:,2])
     y_pred_f = K.flatten(y_pred[:,:,:,2])
@@ -24,6 +27,7 @@ def dice_coef_2(y_true, y_pred,smooth=0.000001):
 #    intersection = K.sum(y_true_f * y_pred_f)
 #    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
+# For Enhancing Tumor (ET):
 def dice_coef_4(y_true, y_pred,smooth=0.000001):
     y_true_f = K.flatten(y_true[:,:,:,4])
     y_pred_f = K.flatten(y_pred[:,:,:,4])
@@ -31,7 +35,7 @@ def dice_coef_4(y_true, y_pred,smooth=0.000001):
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
 # Since label 3 is not there in the BraTS 2019 dataset, we ignore it in the calcuation of Mean Dice Score
-
+# The mean dice score is the mean of the dice scores of all the individual regions.
 def dice_score(y_true, y_pred):
     d0 = dice_coef_0(y_true, y_pred,smooth=0.000001)
     d1 = dice_coef_1(y_true, y_pred,smooth=0.000001)
